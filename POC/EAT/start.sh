87d8f52e0f8653cb9b84da11e56af6b5a070c17b
unknown <lixinxiao02@msn.com>
2018-04-26 15:45:44 +0800

    NA

--- POC/EAT/start.sh
+++ POC/EAT/start.sh
@@ -11,7 +11,8 @@ installPackageOnContainer()
 launchApp(){
     cd /usr/local/eat
     # python3 app.py
-    flask run --host=0.0.0.0 --port=8080
+    python3 -m flask run --host=0.0.0.0 --port=8080
+
 }
 
 echo "===> Installing required python packages"
